cb4c6c9a4aece1092485b86efd8d24abd25cac73
Mark Bonnici <mb6792@gmail.com>
2020-02-18 14:32:50 +0100

    VIEW_REGISTER .env variable

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,14 +1,16 @@
 <template>
   <div class>
-    <div class="container-fluid" v-if="false">
+    <div v-if="showHeader" class="container-fluid">
       <b-navbar type="light" fixed="top" variant="bg-colored" class="">
         <b-container>
-          <b-navbar-brand to="/">
-            <img src="/img/logo.svg" width="160" class="d-inline-block align-center" alt="WEAVR" />
-          </b-navbar-brand>
           <b-collapse id="nav_collapse" is-nav>
             <b-navbar-nav class="ml-auto">
-              <b-nav-item to="/register">Register</b-nav-item>
+              <b-nav-item v-if="showRegister" to="/register">
+                Register
+              </b-nav-item>
+              <b-nav-item v-if="showLogin" to="/login">
+                Login
+              </b-nav-item>
             </b-navbar-nav>
           </b-collapse>
         </b-container>
@@ -22,6 +24,7 @@
 <script lang="ts">
 import { Component } from 'nuxt-property-decorator'
 import { VueWithRouter } from '~/base/classes/VueWithRouter'
+import config from '~/config'
 
 @Component({
   components: {
@@ -36,6 +39,24 @@ import { VueWithRouter } from '~/base/classes/VueWithRouter'
 class AuthLayout extends VueWithRouter {
   get isLogin(): boolean {
     return this.$route.path === '/login'
+  }
+
+  get showHeader(): boolean {
+    return config.app.view_register
+  }
+
+  get showRegister(): boolean {
+    const _matchedName = this.$route.matched[0].name
+    if (_matchedName) {
+      const _registration = ['register']
+      return !_registration.includes(_matchedName)
+    } else {
+      return false
+    }
+  }
+
+  get showLogin(): boolean {
+    return !this.showRegister
   }
 }
 
