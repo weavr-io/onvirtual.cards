825fb07d7646ba3ef14042254d4b89c0b8c2e0a5
Mark Bonnici <mb6792@gmail.com>
2020-03-09 17:50:36 +0100

    fixed error message

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -13,7 +13,7 @@
 
             <form id="contact-form" @submit="login" class="mt-5">
               <error-alert
-                message="Incorrect email and password combination.  If you do not have an account please click on Register."
+                message="Incorrect username and password combination. If you do not have an account please click on Register."
               />
               <b-form-group id="ig-code" label="USERNAME:" label-for="form-code">
                 <b-form-input id="from-code" v-model="loginRequest.code" class="form-control" name="setCode" placeholder="Username" />
