5e52f8f771854eb2173e8a2a25802c32b2210ebc
Mark Bonnici <mb6792@gmail.com>
2020-04-02 11:33:58 +0200

    kyc text

--- pages/managed-accounts/kyc/failed.vue
+++ pages/managed-accounts/kyc/failed.vue
@@ -1,22 +1,16 @@
 <template>
   <section>
     <b-container>
-      <b-row>
-        <b-col>
-          <h2 class="text-center font-weight-lighter">
-            KYC Failed
-          </h2>
-        </b-col>
-      </b-row>
       <b-row class="py-5 my-5 text-center">
-        <b-col>
+        <b-col md="6" offset-md="3">
           <b-row>
             <b-col>
-              <h4 class="font-weight-light">
-                Automatic KYC Failed
-              </h4>
+              <h2 class="font-weight-light">
+                KYC Failed
+              </h2>
               <h5 class="font-weight-lighter">
-                Text here
+                We're sorry but we could not verify your documents automatically. These documents will now be manually
+                reviewed by one of our operators - this may take up to 15 minutes.
               </h5>
             </b-col>
           </b-row>
