504c2795a54a0bf81b48b1b56f47771f0dd86c3d
Mark Bonnici <mb6792@gmail.com>
2020-04-09 19:40:50 +0200

    error vue

--- layouts/error.vue
+++ layouts/error.vue
@@ -3,12 +3,10 @@
     <b-row class="full-height-vh" align-v="center">
       <b-col class="text-center" lg="6" offset-lg="3">
         <template v-if="is404">
-          <svg xmlns="http://www.w3.org/2000/svg" width="90" height="90" fill="#232A47" viewBox="0 0 48 48">
-            <path d="M22 30h4v4h-4zm0-16h4v12h-4zm1.99-10C12.94 4 4 12.95 4 24s8.94 20 19.99 20S44 35.05 44 24 35.04 4 23.99 4zM24 40c-8.84 0-16-7.16-16-16S15.16 8 24 8s16 7.16 16 16-7.16 16-16 16z" />
-          </svg>
-          <h1 class="font-weight-light mt-3 mb-5">
+          <h1 class="font-weight-light">
             Page not found.
           </h1>
+          <b-img src="/img/ohsnap.svg" fluid class="mt-5 mb-4"></b-img>
           <b-button variant="secondary" to="/">
             go to dashboard
           </b-button>
