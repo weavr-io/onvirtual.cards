f0e69667d9ef062f399717fea68205db32038cba
Mark Bonnici <mb6792@gmail.com>
2020-04-22 16:39:38 +0200

    kyc check on idenfy success moved to another page

--- pages/managed-accounts/kyc/index.vue
+++ pages/managed-accounts/kyc/index.vue
@@ -48,46 +48,8 @@ export default class KycPage extends VueWithRouter {
         this.$router.push('/managed-accounts/kyc/failed')
         break
       case 'approved':
-        this.KycApproved()
+        this.$router.push('/managed-accounts/kyc/check')
         break
-    }
-  }
-
-  private tries: number = 0
-
-  async KycApproved() {
-    const _id = AuthStore.Helpers.identityId(this.$store)
-    if (_id != null) {
-      const _consumer = await ConsumersStore.Helpers.get(this.$store, _id)
-
-      if (_consumer.data.kyc?.fullDueDiligence === FullDueDiligence.APPROVED) {
-        this.redirectToAccountPage()
-      } else {
-        this.tries++
-
-        if (this.tries > 3) {
-          this.$bvModal
-            .msgBoxOk('It seems to be taking more than usual. Please refresh the page in a few minutes.', {
-              title: 'Something is wrong.',
-              centered: true
-            })
-            .then(() => {
-              this.redirectToAccountPage()
-            })
-        } else {
-          await this.sleep(5000)
-          this.KycApproved()
-        }
-      }
-    }
-  }
-
-  async redirectToAccountPage() {
-    const _accounts = await AccountsStore.Helpers.index(this.$store)
-
-    if (_accounts.data.count === 1) {
-      const _accountId = _accounts.data.account[0].id.id
-      this.$router.push('/managed-accounts/' + _accountId)
     }
   }
 
