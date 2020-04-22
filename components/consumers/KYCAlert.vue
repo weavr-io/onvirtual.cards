c515c3d97cec590d87e571cbe89606521ec88127
Mark Bonnici <mb6792@gmail.com>
2020-04-22 16:42:29 +0200

    fixed kyc alert

--- components/consumers/KYCAlert.vue
+++ components/consumers/KYCAlert.vue
@@ -3,25 +3,25 @@
     <b-container>
       <b-row>
         <b-col md="6" offset-md="3" class="py-3 font-weight-lighter">
-        <!-- <b-col class="py-5 text-center"> -->
+          <!-- <b-col class="py-5 text-center"> -->
           <div>
-          <h3 class="text-center font-weight-lighter mb-4">
-            We need some documents
-          </h3>
-          <p>
-            We are required by financial services regulations to perform due diligence before allowing
-            you to transact with your account.
-          </p>
-          <p>
-            Kindly prepare to submit and upload the following information:
-          </p>
-          <ul class="my-3 font-weight-normal">
-            <li>An ID document (Passport, National ID Card, Residence Permit)</li>
-            <li>Take a selfie</li>
-            <li>A utility bill showing your residential address</li>
-          </ul>
+            <h3 class="text-center font-weight-lighter mb-4">
+              We need some documents
+            </h3>
+            <p>
+              We are required by financial services regulations to perform due diligence before allowing you to transact
+              with your account.
+            </p>
+            <p>
+              Kindly prepare to submit and upload the following information:
+            </p>
+            <ul class="my-3 font-weight-normal">
+              <li>An ID document (Passport, National ID Card, Residence Permit)</li>
+              <li>Take a selfie</li>
+              <li>A utility bill showing your residential address</li>
+            </ul>
 
-            <h5 class="mt-4">
+            <h5 class="mt-4 text-center">
               <b-link to="/managed-accounts/kyc" class="link pb-2">
                 Start verification process
               </b-link>
