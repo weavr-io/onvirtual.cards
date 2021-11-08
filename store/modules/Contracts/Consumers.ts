60a716166d8befc9ecdd3b4cc1e0e6dd2e97e48f
Joshua <joshua@threls.com>
2021-11-08 13:23:47 +0100

    fix(consumerStore): add consumer store and remove old store

--- store/modules/Contracts/Consumers.ts
+++ store/modules/Contracts/Consumers.ts
@@ -1,76 +1,76 @@
-import { Consumer } from '~/api/Models/Consumers/Consumer'
-import { ActionContext, ActionTree, Store } from '~/node_modules/vuex'
-import { AxiosPromise } from '~/node_modules/axios'
-import { CreateConsumerRequest } from '~/api/Requests/Consumers/CreateConsumerRequest'
-import { StoreHelpers } from '~/helpers/StoreHelpers'
-import { SendVerificationEmail } from '~/api/Requests/Consumers/SendVerificationEmail'
-import { SendVerificationMobile } from '~/api/Requests/Consumers/SendVerificationMobile'
-import { VerifyMobileRequest } from '~/api/Requests/Consumers/VerifyMobileRequest'
-import { InitFullKycProcessResponse } from '~/api/Responses/Consumers/InitFullKycProcessResponse'
-import { UpdateConsumerRequest } from '~/api/Requests/Consumers/UpdateConsumerRequest'
-
-export const name = 'consumers'
-
-export const namespaced = true
-
-export const types = {
-  SET_IS_LOADING: 'SET_IS_LOADING',
-  SET_CONSUMER: 'SET_CONSUMER'
-}
-
-export interface State {
-  isLoading: boolean
-  consumer: Consumer | null
-}
-
-export enum _Actions {
-  create = 'create',
-  update = 'update',
-  get = 'get',
-  sendVerificationCodeEmail = 'sendVerificationCodeEmail',
-  sendVerificationCodeMobile = 'sendVerificationCodeMobile',
-  verifyMobile = 'verifyMobile',
-  checkKYC = 'checkKYC',
-  startKYC = 'startKYC'
-}
-
-export interface Actions<S, R> extends ActionTree<S, R> {
-  [_Actions.create](context: ActionContext<S, R>, request: CreateConsumerRequest): AxiosPromise<Consumer>
-  [_Actions.update](context: ActionContext<S, R>, request: UpdateConsumerRequest): AxiosPromise<Consumer>
-  [_Actions.get](context: ActionContext<S, R>, id: number): AxiosPromise<Consumer>
-  [_Actions.sendVerificationCodeEmail](context: ActionContext<S, R>, request: SendVerificationEmail): AxiosPromise
-  [_Actions.sendVerificationCodeMobile](context: ActionContext<S, R>, request: SendVerificationMobile): AxiosPromise
-  [_Actions.verifyMobile](context: ActionContext<S, R>, request: VerifyMobileRequest): AxiosPromise
-  [_Actions.checkKYC](context: ActionContext<S, R>): Promise<any>
-  [_Actions.startKYC](context: ActionContext<S, R>, consumerId): AxiosPromise<InitFullKycProcessResponse>
-}
-
-export module Helpers {
-  export const consumer = (store: Store<any>): Consumer | null => {
-    return StoreHelpers.get(store, name, 'consumer')
-  }
-  export const create = (store: Store<any>, request: CreateConsumerRequest): AxiosPromise<Consumer> => {
-    return StoreHelpers.dispatch(store, name, _Actions.create, request)
-  }
-  export const update = (store: Store<any>, request: UpdateConsumerRequest): AxiosPromise<Consumer> => {
-    return StoreHelpers.dispatch(store, name, _Actions.update, request)
-  }
-  export const get = (store: Store<any>, id: number): AxiosPromise<Consumer> => {
-    return StoreHelpers.dispatch(store, name, _Actions.get, id)
-  }
-  export const sendVerificationCodeEmail = (store: Store<any>, request: SendVerificationEmail): AxiosPromise => {
-    return StoreHelpers.dispatch(store, name, _Actions.sendVerificationCodeEmail, request)
-  }
-  export const sendVerificationCodeMobile = (store: Store<any>, request: SendVerificationMobile): AxiosPromise => {
-    return StoreHelpers.dispatch(store, name, _Actions.sendVerificationCodeMobile, request)
-  }
-  export const verifyMobile = (store: Store<any>, request: VerifyMobileRequest): AxiosPromise => {
-    return StoreHelpers.dispatch(store, name, _Actions.verifyMobile, request)
-  }
-  export const checkKYC = (store: Store<any>): Promise<any> => {
-    return StoreHelpers.dispatch(store, name, _Actions.checkKYC)
-  }
-  export const startKYC = (store: Store<any>, consumerId): AxiosPromise<InitFullKycProcessResponse> => {
-    return StoreHelpers.dispatch(store, name, _Actions.startKYC, consumerId)
-  }
-}
+// import { Consumer } from '~/api/Models/Consumers/Consumer'
+// import { ActionContext, ActionTree, Store } from '~/node_modules/vuex'
+// import { AxiosPromise } from '~/node_modules/axios'
+// import { CreateConsumerRequest } from '~/api/Requests/Consumers/CreateConsumerRequest'
+// import { StoreHelpers } from '~/helpers/StoreHelpers'
+// import { SendVerificationEmail } from '~/api/Requests/Consumers/SendVerificationEmail'
+// import { SendVerificationMobile } from '~/api/Requests/Consumers/SendVerificationMobile'
+// import { VerifyMobileRequest } from '~/api/Requests/Consumers/VerifyMobileRequest'
+// import { InitFullKycProcessResponse } from '~/api/Responses/Consumers/InitFullKycProcessResponse'
+// import { UpdateConsumerRequest } from '~/api/Requests/Consumers/UpdateConsumerRequest'
+//
+// export const name = 'consumers'
+//
+// export const namespaced = true
+//
+// export const types = {
+//   SET_IS_LOADING: 'SET_IS_LOADING',
+//   SET_CONSUMER: 'SET_CONSUMER'
+// }
+//
+// export interface State {
+//   isLoading: boolean
+//   consumer: Consumer | null
+// }
+//
+// export enum _Actions {
+//   create = 'create',
+//   update = 'update',
+//   get = 'get',
+//   sendVerificationCodeEmail = 'sendVerificationCodeEmail',
+//   sendVerificationCodeMobile = 'sendVerificationCodeMobile',
+//   verifyMobile = 'verifyMobile',
+//   checkKYC = 'checkKYC',
+//   startKYC = 'startKYC'
+// }
+//
+// export interface Actions<S, R> extends ActionTree<S, R> {
+//   [_Actions.create](context: ActionContext<S, R>, request: CreateConsumerRequest): AxiosPromise<Consumer>
+//   [_Actions.update](context: ActionContext<S, R>, request: UpdateConsumerRequest): AxiosPromise<Consumer>
+//   [_Actions.get](context: ActionContext<S, R>, id: number): AxiosPromise<Consumer>
+//   [_Actions.sendVerificationCodeEmail](context: ActionContext<S, R>, request: SendVerificationEmail): AxiosPromise
+//   [_Actions.sendVerificationCodeMobile](context: ActionContext<S, R>, request: SendVerificationMobile): AxiosPromise
+//   [_Actions.verifyMobile](context: ActionContext<S, R>, request: VerifyMobileRequest): AxiosPromise
+//   [_Actions.checkKYC](context: ActionContext<S, R>): Promise<any>
+//   [_Actions.startKYC](context: ActionContext<S, R>, consumerId): AxiosPromise<InitFullKycProcessResponse>
+// }
+//
+// export module Helpers {
+//   export const consumer = (store: Store<any>): Consumer | null => {
+//     return StoreHelpers.get(store, name, 'consumer')
+//   }
+//   export const create = (store: Store<any>, request: CreateConsumerRequest): AxiosPromise<Consumer> => {
+//     return StoreHelpers.dispatch(store, name, _Actions.create, request)
+//   }
+//   export const update = (store: Store<any>, request: UpdateConsumerRequest): AxiosPromise<Consumer> => {
+//     return StoreHelpers.dispatch(store, name, _Actions.update, request)
+//   }
+//   export const get = (store: Store<any>, id: number): AxiosPromise<Consumer> => {
+//     return StoreHelpers.dispatch(store, name, _Actions.get, id)
+//   }
+//   export const sendVerificationCodeEmail = (store: Store<any>, request: SendVerificationEmail): AxiosPromise => {
+//     return StoreHelpers.dispatch(store, name, _Actions.sendVerificationCodeEmail, request)
+//   }
+//   export const sendVerificationCodeMobile = (store: Store<any>, request: SendVerificationMobile): AxiosPromise => {
+//     return StoreHelpers.dispatch(store, name, _Actions.sendVerificationCodeMobile, request)
+//   }
+//   export const verifyMobile = (store: Store<any>, request: VerifyMobileRequest): AxiosPromise => {
+//     return StoreHelpers.dispatch(store, name, _Actions.verifyMobile, request)
+//   }
+//   export const checkKYC = (store: Store<any>): Promise<any> => {
+//     return StoreHelpers.dispatch(store, name, _Actions.checkKYC)
+//   }
+//   export const startKYC = (store: Store<any>, consumerId): AxiosPromise<InitFullKycProcessResponse> => {
+//     return StoreHelpers.dispatch(store, name, _Actions.startKYC, consumerId)
+//   }
+// }
--- utils/store-accessor.ts
+++ utils/store-accessor.ts
@@ -5,6 +5,8 @@ import Accounts from '~/store/accounts'
 import Corporates from '~/store/corporates'
 import Loader from '~/store/loader'
 import Auth from '~/store/auth'
+import SecureClient from '~/store/secureClient'
+import Consumers from '~/store/consumers'
 
 export interface Stores {
   cards: Cards
@@ -12,6 +14,8 @@ export interface Stores {
   corporates: Corporates
   loader: Loader
   auth: Auth
+  secureClient: SecureClient
+  consumers: Consumers
 }
 
 function initialiseStores(store: Store<any>): Stores {
@@ -20,8 +24,18 @@ function initialiseStores(store: Store<any>): Stores {
     accounts: accountsStore(store),
     corporates: corporatesStore(store),
     loader: loaderStore(store),
-    auth: authStore(store)
+    auth: authStore(store),
+    secureClient: secureClientStore(store),
+    consumers: consumersStore(store)
   }
+}
+
+function consumersStore(store: Store<any>) {
+  return getModule(Consumers, store)
+}
+
+function secureClientStore(store: Store<any>) {
+  return getModule(SecureClient, store)
 }
 
 function cardsStore(store: Store<any>) {
@@ -44,4 +58,17 @@ function loaderStore(store: Store<any>) {
   return getModule(Loader, store)
 }
 
-export { initialiseStores, cardsStore, accountsStore, corporatesStore, loaderStore, authStore }
+// function viewStore(store: Store<any>) {
+//   return getModule(View, store)
+// }
+
+export {
+  initialiseStores,
+  cardsStore,
+  accountsStore,
+  corporatesStore,
+  loaderStore,
+  authStore,
+  secureClientStore,
+  consumersStore
+}
