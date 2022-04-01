49f094ac7c209384a7bce4aebd31c11e2a012311
Joshua <joshua@threls.com>
2022-04-01 12:31:08 +0200

    fix(ValidationMixin.ts): extract constant from function

--- minixs/ValidationMixin.ts
+++ minixs/ValidationMixin.ts
@@ -1,4 +1,5 @@
 import { Component, Vue } from 'nuxt-property-decorator'
+import { INVALID_FEEDBACK_CONST } from '~/models/local/constants/InvalidFeedbackConst'
 
 @Component
 export default class ValidationMixin extends Vue {
@@ -26,29 +27,7 @@ export default class ValidationMixin extends Vue {
   }
 
   public invalidFeedback(_item, type, customMessage) {
-    const validationMessage: Record<string, string> = {
-      required: 'This is a required field.',
-      url: 'Please make sure that it is a valid URL.',
-      terms: 'Please accept terms and conditions.',
-      email: 'Please enter a valid email address.',
-      password:
-        'Please enter a valid password - must be between 8 and 30 characters, and must include lowercase, uppercase, number and special characters.',
-      newPassword:
-        'Please enter a valid password - must be between 8 and 30 characters, must be different from the current password and must include lowercase, uppercase, number and special characters.',
-      confirmPassword: 'Confirmation password should be the same as password.',
-      numeric: 'Field must be numeric.',
-      maxLength: 'This field should not exceed 50 characters.',
-      minLength: 'This field requires at least 6 characters.',
-      baseRegexValidation: 'This field contains invalid characters.',
-      name: 'This field contains invalid characters.',
-      surname: 'This field contains invalid characters.',
-      tag: 'This field contains invalid characters.',
-      mobileNumber: 'This field contains invalid characters.',
-      feeKey: 'This field contains invalid characters.',
-      minValue: 'Please enter a greater value.'
-    }
-
-    const messageResult: string | undefined = customMessage ?? validationMessage[type]
+    const messageResult: string | undefined = customMessage ?? INVALID_FEEDBACK_CONST[type]
 
     return messageResult ?? 'Not Handled'
   }
