129d1788d2ecae7b99a1e6825c181c97c1093064
Marc Finger <79254875+fingm-t@users.noreply.github.com>
2023-01-17 10:09:11 +0100

    DEV-4217 onvirtual.cards - Update Node JS from v14 to v16 (#111)
    * build(package.json): update to node16.16.0 & dependencies
    
    * fix(.eslintrc.js): eslint.js
    
    remove unncessary prettier/vue
    
    * build(Dockerfile-app): update docker to ude node16
    
    * build(package.json): move dependencies to devdependencies
    
    * build(package-lock.json): create package-lock using npm associated with node version

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,17 +2,17 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
-    parser: '@typescript-eslint/parser'
+    parser: '@typescript-eslint/parser',
   },
-  extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'plugin:prettier/recommended', 'prettier', 'prettier/vue'],
+  extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'plugin:prettier/recommended', 'prettier'],
   plugins: ['prettier', '@typescript-eslint'],
   // add your custom rules here
   rules: {
     '@typescript-eslint/no-unused-vars': 'error',
     '@typescript-eslint/no-console': 0,
-    'no-console': 0
-  }
+    'no-console': 0,
+  },
 }
