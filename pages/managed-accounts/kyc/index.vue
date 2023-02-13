bb5062b72360d67137506ae85e3b56ac3e2552fd
Jason Vella <jason@threls.com>
2023-02-13 15:33:08 +0100

    feat(kyc/index.vue): Display info for KYC RECECTED and KYC PENDING REVIEW

--- pages/managed-accounts/kyc/index.vue
+++ pages/managed-accounts/kyc/index.vue
@@ -1,5 +1,6 @@
 <template>
   <section>
+    <dashboard-header />
     <b-container>
       <b-row>
         <b-col>
@@ -9,6 +10,16 @@
                 <b-spinner />
               </div>
             </div>
+          </template>
+          <template v-else-if="isKycPending">
+            <div class="pt-5">
+              <KYCAlert />
+            </div>
+          </template>
+          <template v-else-if="isKycRejected">
+            <h3>Issues with your due diligence</h3>
+            <p>The submitted documents and information were rejected.</p>
+            <small class="text-muted">{{ kycErrorCode }}</small>
           </template>
           <template v-else>
             <weavr-kyc :reference="reference" :options="options" />
@@ -23,41 +34,49 @@ import { Component, mixins } from 'nuxt-property-decorator'
 import BaseMixin from '~/mixins/BaseMixin'
 import WeavrKyc from '~/plugins/weavr/components/WeavrKyc.vue'
 import { ConsumerVerificationFlowOptions } from '~/plugins/weavr/components/api'
+import KYCAlert from '~/components/consumers/KYCAlert.vue'
+import DashboardHeader from '~/components/DashboardHeader.vue'
+import { KYCErrorCodeEnum } from '~/plugins/weavr-multi/api/models/identities/consumers/enums/KYCErrorCodeEnum'
 
 @Component({
-  components: { WeavrKyc },
-  middleware: ['kyVerified']
+  components: { DashboardHeader, KYCAlert, WeavrKyc },
+  middleware: ['kyVerified'],
 })
 export default class KycPage extends mixins(BaseMixin) {
   redirectUrl!: string
 
   reference!: string
 
+  kycErrorCode?: KYCErrorCodeEnum | null = null
+
+  get isKycPending() {
+    return this.kycErrorCode === KYCErrorCodeEnum.KYC_PENDING_REVIEW
+    // return this.stores.consumers.kyc?.fullDueDiligence === KYCStatusEnum.PENDING_REVIEW
+  }
+
+  get isKycRejected() {
+    return this.kycErrorCode === KYCErrorCodeEnum.KYC_REJECTED
+  }
+
   fetch() {
-    return this.stores.consumers.startKYC().then((res) => {
-      this.$weavrSetUserToken('Bearer ' + this.stores.auth.token)
-      this.reference = res.data.reference
-    })
+    return this.stores.consumers
+      .startKYC()
+      .then((res) => {
+        this.$weavrSetUserToken('Bearer ' + this.stores.auth.token)
+        this.reference = res.data.reference
+      })
+      .catch((res) => {
+        this.kycErrorCode = res.response.data.errorCode
+      })
   }
 
   options: Partial<ConsumerVerificationFlowOptions> = {
-    onMessage: this.onMessage
+    onMessage: this.onMessage,
   }
 
   onMessage(message, additionalInfo) {
     if (message === 'kycSubmitted') {
       this.$router.push('/managed-accounts/kyc/check')
-    }
-  }
-
-  receiveMessage(event) {
-    switch (event.data.status) {
-      case 'failed':
-        this.$router.push('/managed-accounts/kyc/failed')
-        break
-      case 'approved':
-        this.$router.push('/managed-accounts/kyc/check')
-        break
     }
   }
 }
