c8ad06592697b265bc5fb994caf0839b3cffb22d
Jason Vella <jason@threls.com>
2023-03-23 13:26:17 +0100

    fix(profile/mobile/index.vue): added index page in path with redirection to dashboard

---
+++ pages/profile/mobile/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { Component, Vue } from 'nuxt-property-decorator'
+import { identitiesStore } from '@/utils/store-accessor'
+
+@Component({})
+export default class Mobile extends Vue {
+  asyncData({ redirect, store }) {
+    if (identitiesStore(store).mobileNumberVerified) {
+      return redirect('/dashboard')
+    }
+  }
+}
+</script>
