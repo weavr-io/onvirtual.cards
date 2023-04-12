e6c89ac78bbad23638355a9e46d84396a9f35add
Jason Vella <jason@threls.com>
2023-04-12 15:15:01 +0200

    style(KYBAlert.vue): styling fixes

--- components/corporates/KYBAlert.vue
+++ components/corporates/KYBAlert.vue
@@ -1,28 +1,20 @@
 <template>
   <section>
     <b-container>
-      <b-row v-if="isPendingReview">
-        <b-col md="6" offset-md="3" class="py-3 font-weight-lighter text-center">
-          <h3 class="font-weight-lighter mb-4">
-            Your account is currently under review.
-          </h3>
-          <p>
-            This process normally takes a few days.
-          </p>
+      <b-row v-if="isPendingReview" align-h="center">
+        <b-col §class="py-3 font-weight-lighter text-center">
+          <h3 class="font-weight-lighter mb-4">Your account is currently under review.</h3>
+          <p>This process normally takes a few days.</p>
         </b-col>
       </b-row>
-      <b-row v-else>
-        <b-col md="6" offset-md="3" class="py-3 font-weight-lighter">
-          <h3 class="text-center font-weight-lighter mb-4">
-            We need some documents
-          </h3>
+      <b-row v-else align-h="center">
+        <b-col md="9" lg="6" class="py-3 font-weight-lighter">
+          <h3 class="text-center font-weight-lighter mb-4">We need some documents</h3>
           <p>
             We are required by financial services regulations to perform due diligence on your company before allowing
             you to transact with your account.
           </p>
-          <p>
-            Kindly prepare to submit and upload the following documents:
-          </p>
+          <p>Kindly prepare to submit and upload the following documents:</p>
           <ul class="my-3 font-weight-normal">
             <li>Copy of the Certificate of Incorporation</li>
             <li>Copy of the Articles of Association (last amendment)</li>
@@ -33,17 +25,13 @@
             </li>
             <li>Commercial registry extract showing shareholders and directors</li>
           </ul>
-          <p>
-            You will also need to provide the following information:
-          </p>
+          <p>You will also need to provide the following information:</p>
           <ul class="my-3 font-weight-normal">
             <li>Directors: name, date of birth, nationality, email address, contact number</li>
             <li>Ultimate Beneficial Owners (UBOs) holding a stake of 25% or more: name, date of birth, nationality</li>
           </ul>
-          <div class="text-center">
-            <b-button to="/managed-accounts/kyb">
-              start the verification process
-            </b-button>
+          <div class="text-center mt-5">
+            <b-button to="/managed-accounts/kyb"> start the verification process</b-button>
           </div>
         </b-col>
       </b-row>
