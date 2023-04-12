8993c680361bee8017565cf3f6e401fe907c09a6
Jason Vella <jason@threls.com>
2023-04-12 15:12:55 +0200

    style(KYCAlert.vue): styling fixes

--- components/consumers/KYCAlert.vue
+++ components/consumers/KYCAlert.vue
@@ -1,30 +1,24 @@
 <template>
   <section>
     <b-container>
-      <b-row v-if="isPendingReview">
-        <b-col md="6" offset-md="3" class="py-3 font-weight-lighter text-center">
-          <h3 class="font-weight-lighter mb-4">
-            Your documentation is currently under review.
-          </h3>
+      <b-row v-if="isPendingReview" align-h="center">
+        <b-col md="9" lg="6" class="py-3 font-weight-lighter text-center">
+          <h3 class="font-weight-lighter mb-4">Your documentation is currently under review.</h3>
           <p>
             This process normally takes a few minutes. During busy times, review of documentation may take a bit longer
             up to 24 hours.
           </p>
         </b-col>
       </b-row>
-      <b-row v-else>
-        <b-col md="6" offset-md="3" class="py-3 font-weight-lighter">
+      <b-row v-else align-h="center">
+        <b-col md="9" lg="6" class="py-3 font-weight-lighter">
           <div>
-            <h3 class="text-center font-weight-lighter mb-4">
-              We need some documents
-            </h3>
+            <h3 class="text-center font-weight-lighter mb-4">We need some documents</h3>
             <p>
               We are required by financial services regulations to perform due diligence before allowing you to transact
               with your account.
             </p>
-            <p>
-              Kindly prepare to submit and upload the following information:
-            </p>
+            <p>Kindly prepare to submit and upload the following information:</p>
             <ul class="my-3 font-weight-normal">
               <li>An ID document (Passport or National ID Card)</li>
               <li>Take a selfie</li>
@@ -32,9 +26,7 @@
             </ul>
 
             <h5 class="mt-4 text-center">
-              <b-link to="/managed-accounts/kyc" class="link pb-2">
-                Start verification process
-              </b-link>
+              <b-link to="/managed-accounts/kyc" class="link pb-2"> Start verification process</b-link>
             </h5>
           </div>
         </b-col>
