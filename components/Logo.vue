031b933e92e552f6bbe4abe4f22ef08c96cf2f2f
Jason Vella <jason@threls.com>
2023-04-12 15:57:45 +0200

    feat(Logo.vue): added new prop for classes

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="text-center mb-5">
+  <div class="text-center" :class="baseClass">
     <b-link to="/">
       <img src="/img/logo.svg" width="200" class="d-inline-block align-top" alt="onvirtual.cards" />
     </b-link>
   </div>
 </template>
 <script lang="ts">
-import { Component, Vue } from 'nuxt-property-decorator'
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
 
 @Component
-export default class Logo extends Vue {}
+export default class Logo extends Vue {
+  @Prop(String) baseClass?: string
+}
 </script>
