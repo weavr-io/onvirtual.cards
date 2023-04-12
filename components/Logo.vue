d36ff0aa71eaa74c85d273a2d57c67dc4682d92c
Jason Vella <jason@threls.com>
2023-04-12 15:06:42 +0200

    feat(logo.vue): new component for logo

---
+++ components/Logo.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="text-center mb-5">
+    <b-link to="/">
+      <img src="/img/logo.svg" width="200" class="d-inline-block align-top" alt="onvirtual.cards" />
+    </b-link>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class Logo extends Vue {}
+</script>
