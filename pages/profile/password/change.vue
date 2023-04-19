d167ad8b0459d765de4f4772c71e43c596cc274d
Jason Vella <jason@threls.com>
2023-04-19 14:01:24 +0200

    fix(password/change.vue): updated password component

--- pages/profile/password/change.vue
+++ pages/profile/password/change.vue
@@ -2,36 +2,42 @@
   <section class="register-section">
     <b-container>
       <b-row align-h="center">
-        <b-col md="9" lg="6" class="my-5 text-center">
-          <div class="text-md d-inline-block text-center mb-6">
+        <b-col md="9" lg="6" class="my-2 my-lg-5 text-center">
+          <div class="text-md d-inline-block text-center mb-5 mb-lg-6">
             <h1 class="font-weight-light">Change Password</h1>
           </div>
           <div class="mx-md-3 px-md-5">
-            <error-alert />
             <b-form id="contact-form" @submit.prevent="submitChangePassword">
               <client-only placeholder="Loading...">
                 <div :class="{ 'is-dirty': $v.form.$dirty }">
                   <label class="d-block text-left">OLD PASSWORD:</label>
                   <weavr-password-input
                     ref="oldPassword"
-                    :options="{ placeholder: '****', classNames: { empty: 'is-invalid', invalid: 'is-invalid' } }"
+                    :options="{ placeholder: '****', classNames: { invalid: 'is-invalid' } }"
                     :base-style="passwordBaseStyle"
                     class-name="sign-in-password"
                     name="old-password"
                     required="true"
                     @onKeyUp.prevent="checkOnKeyUp"
+                    @onChange="oldPasswordInteraction"
                   />
                   <label class="d-block text-left mt-3">NEW PASSWORD:</label>
                   <weavr-password-input
                     ref="newPassword"
-                    :options="{ placeholder: '****', classNames: { empty: 'is-invalid', invalid: 'is-invalid' } }"
+                    :options="{ placeholder: '****', classNames: { invalid: 'is-invalid' } }"
                     :base-style="passwordBaseStyle"
                     class-name="sign-in-password"
                     name="new-password"
                     required="true"
                     @onKeyUp.prevent="checkOnKeyUp"
+                    @onChange="passwordInteraction"
+                    @onStrength="strengthCheck"
                   />
-                  <small class="form-text text-muted text-left">Minimum 8, Maximum 50 characters.</small>
+                  <small
+                    class="form-text mb-3 text-left"
+                    :class="!isPasswordValidAndDirty ? 'text-danger' : 'text-muted'"
+                    >- min 8 characters <br />- uppercase letter <br />- digit and a special character</small
+                  >
                 </div>
               </client-only>
 
@@ -48,6 +54,7 @@
 
 <script lang="ts">
 import { Component, mixins, Ref } from 'nuxt-property-decorator'
+import { required } from 'vuelidate/lib/validators'
 import LoaderButton from '~/components/LoaderButton.vue'
 import { SecureElementStyleWithPseudoClasses } from '~/plugins/weavr/components/api'
 import BaseMixin from '~/mixins/BaseMixin'
@@ -62,6 +69,18 @@ import { UpdatePasswordRequestModel } from '~/plugins/weavr-multi/api/models/aut
   },
   validations: {
     form: {},
+    changePasswordRequest: {
+      oldPassword: {
+        value: {
+          required,
+        },
+      },
+      newPassword: {
+        value: {
+          required,
+        },
+      },
+    },
   },
 })
 export default class BundlesPage extends mixins(BaseMixin) {
@@ -71,7 +90,9 @@ export default class BundlesPage extends mixins(BaseMixin) {
   @Ref('newPassword')
   newPassword!: WeavrPasswordInput
 
-  isLoading = false
+  isLoading: boolean = false
+
+  passwordStrength: number = 0
 
   changePasswordRequest: UpdatePasswordRequestModel = {
     oldPassword: {
@@ -82,6 +103,36 @@ export default class BundlesPage extends mixins(BaseMixin) {
     },
   }
 
+  get isPasswordValid(): boolean {
+    return this.passwordStrength >= 2
+  }
+
+  get isPasswordValidAndDirty(): boolean {
+    return !this.$v.changePasswordRequest.newPassword?.$dirty ? true : this.isPasswordValid
+  }
+
+  get isAnyPasswordEmpty(): boolean {
+    return this.changePasswordRequest.oldPassword.value === '' || this.changePasswordRequest.newPassword.value === ''
+  }
+
+  oldPasswordInteraction(val: { empty: boolean; valid: boolean }) {
+    !val.empty
+      ? (this.changePasswordRequest.oldPassword.value = '******')
+      : (this.changePasswordRequest.oldPassword.value = '')
+    this.$v.changePasswordRequest.$touch()
+  }
+
+  passwordInteraction(val: { empty: boolean; valid: boolean }) {
+    !val.empty
+      ? (this.changePasswordRequest.newPassword.value = '******')
+      : (this.changePasswordRequest.newPassword.value = '')
+    this.$v.changePasswordRequest.$touch()
+  }
+
+  strengthCheck(val) {
+    this.passwordStrength = val.id
+  }
+
   checkOnKeyUp(e) {
     if (e.key === 'Enter') {
       e.preventDefault()
@@ -90,44 +141,45 @@ export default class BundlesPage extends mixins(BaseMixin) {
   }
 
   async submitChangePassword() {
-    this.$v.$touch()
-    if (this.$v.form.$invalid) return
-
     let tokenizedOld = ''
     let tokenizedNew = ''
 
-    this.isLoading = true
+    this.$v.$touch()
 
-    await this.oldPassword.createToken().then((res) => {
-      tokenizedOld = res.tokens['old-password']
-    })
-    await this.newPassword.createToken().then((res) => {
-      tokenizedNew = res.tokens['new-password']
-    })
+    if (this.$v.$invalid) {
+    }
+    if (this.isPasswordValid && !this.isAnyPasswordEmpty) {
+      this.isLoading = true
 
-    if (tokenizedOld && tokenizedNew) {
-      this.changePasswordRequest.oldPassword.value = tokenizedOld
-      this.changePasswordRequest.newPassword.value = tokenizedNew
-      console.debug('ALL Settled', this.changePasswordRequest)
-      this.stores.auth.validatePassword({ password: this.changePasswordRequest.newPassword }).then(() => {
-        this.stores.auth
-          .updatePassword(this.changePasswordRequest)
-          .then(() => {
-            this.showSuccessToast('Password changed successfully')
-            this.$router.push('/profile')
-          })
-          .catch((err) => {
-            const data = err.response.data
-            const error = data.message ? data.message : data.errorCode
-
-            this.showErrorToast(error)
-          })
-          .finally(() => {
-            this.isLoading = false
-          })
+      await this.oldPassword.createToken().then((res) => {
+        tokenizedOld = res.tokens['old-password']
       })
-    } else {
-      return null
+      await this.newPassword.createToken().then((res) => {
+        tokenizedNew = res.tokens['new-password']
+      })
+
+      if (tokenizedOld && tokenizedNew) {
+        this.changePasswordRequest.oldPassword.value = tokenizedOld
+        this.changePasswordRequest.newPassword.value = tokenizedNew
+        console.debug('ALL Settled', this.changePasswordRequest)
+        this.stores.auth.validatePassword({ password: this.changePasswordRequest.newPassword }).then(() => {
+          this.stores.auth
+            .updatePassword(this.changePasswordRequest)
+            .then(() => {
+              this.showSuccessToast('Password changed successfully')
+              this.$router.push('/profile')
+            })
+            .catch((err) => {
+              const data = err.response.data
+              const error = data.message ? data.message : data.errorCode
+
+              this.showErrorToast(error)
+            })
+            .finally(() => {
+              this.isLoading = false
+            })
+        })
+      }
     }
   }
 
