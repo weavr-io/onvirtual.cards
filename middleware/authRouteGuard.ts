3d8d2d4e0cb7bc0ca0657d6fdb5bfa55f4e74a1f
Amith Srikantaiah <amith@threls.com>
2023-09-16 13:45:50 +0530

    fix(auth): added a redirect so /register/verify redirects to /login/verify in authGuard for consumers

--- middleware/authRouteGuard.ts
+++ middleware/authRouteGuard.ts
@@ -3,10 +3,14 @@ import config from '~/config'
 
 const Cookie = process.client ? require('js-cookie') : undefined
 
-const authRouteGuard: Middleware = ({ redirect }) => {
+const authRouteGuard: Middleware = ({ redirect, route }) => {
     const authCookie = Cookie.get(config.ONV_COOKIE_NAME)
 
     if (!authCookie) {
+        const queryParam = route.query
+        if (queryParam.cons && queryParam.email) {
+            return redirect(`/login/verify?email=${queryParam.email}&cons=${queryParam.cons}`)
+        }
         return redirect('/login')
     }
 }
