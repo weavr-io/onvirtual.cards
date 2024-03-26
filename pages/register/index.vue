1d580fe3b0b1cf0a6b3851b0a3738beef2a61745
Peter Oduntan <peteroduntan@Peters-MacBook-Pro.local>
2024-03-26 15:05:06 +0100

    update temporary config

--- pages/register/index.vue
+++ pages/register/index.vue
@@ -2,17 +2,19 @@
     <b-container>
         <div class="d-flex flex-column align-items-center">
             <logo />
-            <access-code-component v-if="$config.public.production && !isAccessCodeValid" />
+            <access-code-component
+                v-if="useRuntimeConfig().public.production && !isAccessCodeValid"
+            />
             <business-or-personal-component v-else />
         </div>
     </b-container>
 </template>
 <script lang="ts">
 import { Component, mixins } from 'nuxt-property-decorator'
-import BaseMixin from '~/mixins/BaseMixin'
 import { authStore } from '~/utils/store-accessor'
 import BusinessOrPersonalComponent from '~/components/registration/BusinessOrPersonalComponent.vue'
 import AccessCodeComponent from '~/components/registration/AccessCodeComponent.vue'
+import BaseMixin from '~/mixins/BaseMixin'
 import Logo from '~/components/Logo.vue'
 
 @Component({
