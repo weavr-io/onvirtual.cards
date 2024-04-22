7f31c1dfee6f3c2b7a0dc192e7839e891238b787
Joshua Gauci <joshua@threls.com>
2024-04-22 16:30:45 +0200

    fix(code-smell): code review suggestions

--- plugins/weavr/types/index.d.ts
+++ plugins/weavr/types/index.d.ts
@@ -4,18 +4,18 @@ import '@nuxt/types'
 declare module 'vue/types/vue' {
     interface Vue {
         $weavrComponents: any
-        $weavrSetUserToken: (token: any) => {}
+        $weavrSetUserToken: (token: unknown) => {}
     }
 }
 
 declare module '@nuxt/types' {
     // nuxtContext.app.$weavrSetUserToken inside asyncData, fetch, plugins, middleware, nuxtServerInit
     interface NuxtAppOptions {
-        $weavrSetUserToken: (token: any) => {}
+        $weavrSetUserToken: (token: unknown) => {}
     }
 
     // nuxtContext.$weavrSetUserToken
     interface Context {
-        $weavrSetUserToken: (token: any) => {}
+        $weavrSetUserToken: (token: unknown) => {}
     }
 }
