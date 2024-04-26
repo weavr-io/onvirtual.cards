027aeccc98bb8457bc9d840db048711202cdf310
Joshua Gauci <joshua@threls.com>
2024-04-26 11:19:48 +0200

    refactor(code-smell): code review suggestion smell

--- components/atoms/TransactionAmount.vue
+++ components/atoms/TransactionAmount.vue
@@ -7,24 +7,6 @@
     </div>
 </template>
 <script lang="ts" setup>
-// import { Component, Prop, Vue } from 'nuxt-property-decorator'
-// import { StatementEntryModel } from '~/plugins/weavr-multi/api/models/managed-instruments/statements/models/StatementEntryModel'
-// import { weavrCurrency, weavrCurrencyWithOperator } from '~/utils/helper'
-
-// @Component({ components: {} })
-// export default class StatementItemAdditionalField extends Vue {
-//     @Prop()
-//     readonly transaction!: StatementEntryModel
-//
-//     get currency() {
-//         return weavrCurrency(this.transaction.transactionAmount)
-//     }
-//
-//     get currencyWithOp() {
-//         return weavrCurrencyWithOperator(this.transaction.sourceAmount)
-//     }
-// }
-
 import { PropType } from '@nuxtjs/composition-api'
 import { computed } from 'vue'
 import { StatementEntryModel } from '~/plugins/weavr-multi/api/models/managed-instruments/statements/models/StatementEntryModel'
