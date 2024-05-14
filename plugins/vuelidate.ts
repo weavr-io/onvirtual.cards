b09286490e6bbb9922837ecb45fed2c0d7e89643
Joshua Gauci <joshua@threls.com>
2024-05-14 16:36:21 +0200

    refactor(vuelidate): remove vuelidate and any references or methods associated with vuelidate

--- plugins/vuelidate.ts
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import { defineNuxtPlugin } from '@nuxtjs/composition-api'
-import Vuelidate from 'vuelidate'
-
-export default defineNuxtPlugin((_) => {
-    // TODO: Update to use nuxtApp context after full migration
-    Vue.use(Vuelidate)
-})
