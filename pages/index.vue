c68143bbb507218cc1b4ba1919136a0521493c4f
oleggrech-t <oleg@threls.com>
2024-05-20 12:28:01 +0200

    using router.replace instead of replace

--- pages/index.vue
+++ pages/index.vue
@@ -12,33 +12,33 @@
 import { useAsync, useRouter } from '@nuxtjs/composition-api'
 import { useStores } from '~/composables/useStores'
 
-const { replace } = useRouter()
+const router = useRouter()
 const { auth, identity } = useStores(['auth', 'identity'])
 
 useAsync(async () => {
     const isLoggedIn = auth?.isLoggedIn
 
     if (!isLoggedIn) {
-        replace('/login')
+        router.replace('/login')
     } else {
-        if (identity!.identityState.identity === null) {
+        if (identity?.identityState.identity === null) {
             await identity!.getIdentity()
         }
 
-        if (!identity!.identityState.emailVerified) {
+        if (!identity?.identityState.emailVerified) {
             const email = window.encodeURIComponent(
                 identity!.identityState.identity!.rootUser?.email,
             )
-            replace(`/login/verify?send=true&email=${email}`)
+            router.replace(`/login/verify?send=true&email=${email}`)
         } else if (!identity!.identityState.mobileNumberVerified) {
-            replace('/login/verify/mobile')
+            router.replace('/login/verify/mobile')
         } else if (
             identity!.identityState.identity &&
             typeof identity!.identityState.identity.rootUser === 'undefined'
         ) {
-            replace('/profile/address')
+            router.replace('/profile/address')
         } else {
-            replace('/dashboard')
+            router.replace('/dashboard')
         }
     }
 })
